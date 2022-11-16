8d73fa5bd5e553b62f9e7cf68874f51c044af424
Abmael <abmael.cont@gmail.com>
2022-11-16 18:13:21 -0300

    feat: finishing off email capture section

--- src/pages/Home/EmailCapture/EmailCapture.tsx
+++ src/pages/Home/EmailCapture/EmailCapture.tsx
@@ -1,9 +1,11 @@
 import * as Yup from "yup"
 import { useFormik } from "formik"
+import { GeneralContainer } from "../../../components"
+import { ReactComponent as ErrorIcon } from "../../../assets/icons/error-icon.svg"
 import "./emailCapture.css"
 
 export const EmailCapture = () => {
-    const formik = useFormik({
+    const formManager = useFormik({
         initialValues: {
             email: '',
         },
@@ -18,25 +20,42 @@ export const EmailCapture = () => {
         }
     })
 
-    return (
-        <section>
-                <p>35.000+ ALREADY JOINED</p>
-                <h2>Stay up-to-date with what we're doing</h2>
-                <form onSubmit={formik.handleSubmit}>
-                    <label htmlFor="email">Email</label>
-                    <input 
-                        name="email"
-                        type="text" 
-                        onChange={formik.handleChange}
-                        onBlur={formik.handleBlur}
-                        value={formik.values.email}
-                    />
-                    {formik.touched.email && formik.errors.email 
-                        ? (<div>{formik.errors.email}</div>) 
-                        : null}
+    const isInvalidEmail = formManager.touched.email && formManager.errors.email;
 
-                    <button type="submit">Contact Us</button>
+    return (
+        <section className="email__capture">
+            <GeneralContainer>
+                <div className="email__capture__body">
+                    <p className="email__capture__text">35.000+ ALREADY JOINED</p>
+                    <h2 className="email__capture__headline heading heading-200">Stay up-to-date with what we're doing</h2>
+                </div>
+                <form
+                    className="form"
+                    onSubmit={formManager.handleSubmit}
+                >
+                    <fieldset className={`form__fieldset ${
+                        isInvalidEmail ? 'form__fieldset--error' : ''}`}
+                    >
+                        <div className='form__input__wrapper'>
+                            <input 
+                                className='form__input'
+                                name="email"
+                                type="text"
+                                placeholder="Enter your email address"
+                                onChange={formManager.handleChange}
+                                onBlur={formManager.handleBlur}
+                                value={formManager.values.email}
+                            />
+                            {isInvalidEmail ? <ErrorIcon /> : null}
+                        </div>
+                        {isInvalidEmail
+                            ? <p className="form__error__message"><i>{formManager.errors.email}</i></p>
+                            : null}
+                    </fieldset>
+                    
+                    <button className="form__submit button button-thick button--primary--red" type="submit">Contact Us</button>
                 </form>
+            </GeneralContainer>
         </section>
     )
 }
